b26fe9729212094080252ad71d227f8b47f4d756
guilhermematos13 <guilhermemoliveira1998@gmail.com>
2022-12-22 19:56:48 -0300

    feat: created routers

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,24 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { createBrowserRouter } from "react-router-dom";
-import "./assets/styles.css";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import './assets/styles.css'
 
-import { Login } from "./pages/Login";
+import { Login } from './pages/Login'
+import { UserList } from './pages/UserList'
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
-    <Login />
-  </React.StrictMode>
-);
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Login />,
+    },
+    {
+        path: '/users',
+        element: <UserList />,
+    },
+])
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+    <React.StrictMode>
+        <RouterProvider router={router} />
+    </React.StrictMode>
+)
