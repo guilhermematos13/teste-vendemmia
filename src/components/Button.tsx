30caaf105fcf102fc3aa52d0d2a9d95dd9646ea9
guilhermematos13 <guilhermemoliveira1998@gmail.com>
2022-12-22 22:34:06 -0300

    feat: created button component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -5,7 +5,7 @@ interface ButtonProps extends React.HTMLProps<HTMLButtonElement> {
 export const Button = ({ name, onClick, type }: ButtonProps) => {
     return (
         <button
-            className="bg-pink-900 text-white"
+            className="rounded-md p-2 px-2 w-full font-bold bg-pink-800 text-white  border-pink-800 hover:bg-white hover:text-pink-800 hover:ease-in duration-150 hover:border border-2 "
             type={type}
             onClick={onClick}
         >
