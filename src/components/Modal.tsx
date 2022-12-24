4ee48fb55338dd6cbe73856896f46978fa560c20
guilhermematos13 <guilhermemoliveira1998@gmail.com>
2022-12-23 21:53:59 -0300

    feat: created loading modal

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -6,6 +6,7 @@ import { format } from 'date-fns'
 import { FaWindowClose } from 'react-icons/fa'
 
 import { UserProps } from '../interfaces/user'
+import { Loading } from './Loading'
 
 interface ModalProps {
     changeModal: () => void
@@ -21,6 +22,7 @@ export const Modal = ({
     if (!openModal) return null
 
     const [userDetails, setUserDetails] = useState<UserProps>()
+    const [isLoading, setIsLoading] = useState(true)
 
     const getUserDetails = () => {
         axios
@@ -29,16 +31,16 @@ export const Modal = ({
                 setUserDetails(response.data)
             })
             .catch(function () {
+                changeModal()
                 toast.error('Algo deu errado!')
             })
             .finally(() => {
-                // setIsLoading(false)
+                setIsLoading(false)
             })
     }
     useEffect(() => {
         getUserDetails()
     }, [])
-    if (!userDetails) return null
     return (
         <div className="flex justify-center items-center fixed w-screen h-screen">
             <div className="bg-purple-900 bg-opacity-95 border-2 border-black rounded-xl p-4 fixed w-1/4 h-auto">
@@ -47,21 +49,30 @@ export const Modal = ({
                     className="text-white cursor-pointer float-right"
                     size={20}
                 />
-                <div className="flex flex-col items-center justify-center h-full">
-                    <img
-                        className="h-auto rounded-xl border border-white"
-                        src={userDetails?.avatar}
-                    />
-                    <p className="text-white font-bold mt-4">
-                        {userDetails?.name}
-                    </p>
-                    <label className="text-white text-lg font-bold mt-2">
-                        Acesso criado em:
-                    </label>
-                    <p className="text-white font-bold">
-                        {format(new Date(userDetails.createdAt), 'dd/MM/yyyy')}
-                    </p>
-                </div>
+                {isLoading ? (
+                    <Loading />
+                ) : (
+                    userDetails && (
+                        <div className="flex flex-col items-center justify-center h-full">
+                            <img
+                                className="h-auto rounded-xl border border-white"
+                                src={userDetails?.avatar}
+                            />
+                            <p className="text-white font-bold mt-4">
+                                {userDetails?.name}
+                            </p>
+                            <label className="text-white text-lg font-bold mt-2">
+                                Acesso criado em:
+                            </label>
+                            <p className="text-white font-bold">
+                                {format(
+                                    new Date(userDetails.createdAt),
+                                    'dd/MM/yyyy'
+                                )}
+                            </p>
+                        </div>
+                    )
+                )}
             </div>
         </div>
     )
